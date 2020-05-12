4097446d7f62703ab1b16c7ec782581a76f6ff47
javier.sepulveda <javier.sepulveda@autentia.com>
2020-05-12 12:32:54 +0200

    Added sonarscanner file.

---
+++ example-react/sonarscanner.js
@@ -0,0 +1,15 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+ 
+sonarqubeScanner(
+  {
+    serverUrl : 'https://sonarqube:9000',
+    token : "",
+    options: {
+      'sonar.projectName': 'My App',
+      'sonar.projectDescription': 'Description for "My App" project...',
+      'sonar.sources': 'dist',
+      'sonar.tests': 'test'
+    }
+  },
+  () => process.exit()
+)
